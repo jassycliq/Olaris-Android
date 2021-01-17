ed4c6e91417a6b576677210ebbf3089d06575e8d
Animazing <Animazing@protonmail.com>
2021-01-17 16:31:33 +0100

    Fix server layout

--- app/src/main/java/tv/olaris/android/ui/serverList/ServerListFragment.kt
+++ app/src/main/java/tv/olaris/android/ui/serverList/ServerListFragment.kt
@@ -1,6 +1,7 @@
 package tv.olaris.android.ui.serverList
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -46,6 +47,7 @@ class ServerListFragment : Fragment() {
         binding.recyclerServerList.layoutManager = LinearLayoutManager(this.requireContext())
         lifecycleScope.launch{
             OlarisApplication.applicationContext().serversRepository.allServers.collect {
+                Log.d("Servers", it.toString())
                 adapter.submitList(it)
             }
         }
